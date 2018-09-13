2c06fe6b007fb1d7089fc2bea5bc0b0eb6bedf22
Aditi1903 <42962073+Aditi1903@users.noreply.github.com>
2018-09-13 18:52:04 +0530

    Tutorial Practice

---
+++ Memberships/Entities/Section.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Memberships.Entities
+{
+    [Table("Session")]
+    public class Section
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+    }
+}
